e073b3a5fb72f6c8b97b2e6db3f431198d1ffd25
MutluBilgee <mutlubilgee@gmail.com>
2023-09-06 08:06:49 +0200

    Spring-07-Spring-MVC-Controller

---
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/user")
+    public String user(){
+
+        //return "/user/userinfo.html";  //go to folder static and look overthere
+        return "/user/userinfo.html"; //provide full path!!
+    }
+
+    @RequestMapping("/mutlu")
+    public String user2(){
+
+        return "/user/mutlu.html";
+    }
+
+
+}
