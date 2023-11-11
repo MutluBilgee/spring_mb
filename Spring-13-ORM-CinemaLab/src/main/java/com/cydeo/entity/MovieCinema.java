9199042c65803c5e1550acb9c6e63e0c9de9b0db
MutluBilgee <mutlubilgee@gmail.com>
2023-11-11 16:43:04 +0100

    Spring13OrmCinemaLab entity updates

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/MovieCinema.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/MovieCinema.java
@@ -1,7 +1,8 @@
 package com.cydeo.entity;
 
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,7 +11,8 @@ import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 public class MovieCinema extends BaseEntity{
 
@@ -24,7 +26,10 @@ public class MovieCinema extends BaseEntity{
     private Cinema cinema;
 
 
-
-
-
+    @Override
+    public String toString() {
+        return "MovieCinema{" +
+                "dateTime=" + dateTime +
+                '}';
+    }
 }
