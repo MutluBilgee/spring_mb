a79690681130ddd9d1caed34d16e764567f4b50e
MutluBilgee <mutlubilgee@gmail.com>
2023-12-06 20:34:19 +0100

    Spring 15 REST & Spring 16 REST - Jackson

--- Spring-16-REST-Jackson/src/main/java/com/cydeo/entity/Ticket.java
+++ Spring-16-REST-Jackson/src/main/java/com/cydeo/entity/Ticket.java
@@ -1,8 +1,7 @@
 package com.cydeo.entity;
 
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,8 +10,7 @@ import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 
 @Entity
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 public class Ticket extends BaseEntity {
 
@@ -22,18 +20,10 @@ public class Ticket extends BaseEntity {
     @Column(columnDefinition = "TIMESTAMP")
     private LocalDateTime dateTime;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     private MovieCinema movieCinema;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     private User userAccount;
 
-    @Override
-    public String toString() {
-        return "Ticket{" +
-                "seatNumber=" + seatNumber +
-                ", rowNumber=" + rowNumber +
-                ", dateTime=" + dateTime +
-                '}';
-    }
 }
